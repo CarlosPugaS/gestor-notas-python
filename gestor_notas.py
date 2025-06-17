54841b4994d50cb3d608160328b3a26b3367ed8d
CarlosPugaS <c.puga26@gmail.com>
2025-06-17 18:28:57 -0400

    Estructura base del gestor de notas

---
+++ gestor_notas.py
@@ -0,0 +1,32 @@
+"""
+Este Gestor de notas permitirá :
+- Crear nota
+- Leer todas las notas
+- Buscar por palabra clave
+- Eliminar nota
+- Guardar en archivo
+"""
+
+
+def agregar_nota():
+    pass
+
+
+def mostrar_notas():
+    pass
+
+
+def buscar_en_notas():
+    pass
+
+
+def eliminar_nota():
+    pass
+
+
+def guardar_en_archivo():
+    pass
+
+
+if __name__ == "__main__":
+    pass
