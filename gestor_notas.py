8a4e75cadd3526f6e4dda35f60a7c0c1e468c53b
CarlosPugaS <c.puga26@gmail.com>
2025-06-18 21:01:45 -0400

    Creación del menú para Gestionar notas

--- gestor_notas.py
+++ gestor_notas.py
@@ -6,15 +6,29 @@ Este Gestor de notas permitirá :
 - Eliminar nota
 - Guardar en archivo
 """
+notas = [{"titulo": "algo", "contenido": "algo"}]
 
 
 def agregar_nota():
-    pass
+    print("\n=== Agregar Nota ===")
+    titulo = input("Titulo de la nota: ").strip()
+    contenido = input("Contenido de la nota : ").strip()
+
+    nota = {
+        "titulo": titulo,
+        "contenido": contenido
+    }
+    notas.append(nota)
+    print("Nota agregada correctamente \n")
 
 
 def mostrar_notas():
-    pass
-
+    resultado = []
+    for nota in notas:
+        for titulo, contenido in nota.items():
+            resultado.append((titulo, contenido))
+    return resultado
+    
 
 def buscar_en_notas():
     pass
@@ -28,5 +42,33 @@ def guardar_en_archivo():
     pass
 
 
-if __name__ == "__main__":
-    pass
+def mostrar_menu():
+
+    print("\n === Gestor de Notas === \n")
+    print("1. Crear una nota")
+    print("2. leer todas las notas")
+    print("3. buscar por palabra clave")
+    print("4. eliminar nota")
+    print("5. Guardar la nota en un archivo")
+    print("6. Para salir")
+
+
+while True:
+    mostrar_menu()
+    opcion = input("Ingresa una opcion: \n")
+    
+    if opcion == "1":
+        pass
+    elif opcion == "2":
+        pass
+    elif opcion == "3":
+        pass
+    elif opcion == "4":
+        pass
+    elif opcion == "5":
+        pass
+    elif opcion == "6":
+        print("Gracias por utilizar el Gestor de notas")
+        break
+    else:
+        print("Opcion invalida, vuelve a intentarlo")
